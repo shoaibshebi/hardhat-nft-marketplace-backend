cfb36d46e127f99226f64929a5d3cee9621361e7
shoaibshebi <shoaib4030891@gmail.com>
2022-08-12 14:13:40 +0500

    add utils/move-blocks, scripts/mint, mine, cancel-item, buy-item

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,7 @@
-const { ethers } = require("hardhat");
+//File consists of with calling contract methods to do the stuff with blockchain
+//like first get contract and then call contract function basicNft.mintNft()
+const { ethers, network } = require("hardhat");
+const { moveBlocks } = require("../utils/move-blocks");
 
 const PRICE = ethers.utils.parseEther("0.1");
 
@@ -7,9 +10,7 @@ async function mintAndList() {
   const nftMarketplace = await ethers.getContract("NftMarketplace");
   console.log("Minting...");
   const mintTx = await basicNft.mintNft();
-  console.log("mintTx: ", mintTx);
   const mintTxReceipt = await mintTx.wait(1);
-  console.log("mintTxReceipt: ", mintTxReceipt);
   const tokenId = mintTxReceipt.events[0].args.tokenId;
 
   console.log("Approving NFT...");
@@ -24,7 +25,13 @@ async function mintAndList() {
     PRICE
   );
   await listTx.wait(1);
-  console.log("Listed !");
+  console.log("NFT Listed!");
+  // if (network.config.chainId == 31337) {
+  // Moralis has a hard time if you move more than 1 at once!
+  console.log("Moving Blocks...");
+  await moveBlocks(1, (sleepAmount = 1000));
+  console.log("Moved!");
+  // }
 }
 
 mintAndList()
